65b99960bc6dccdf92452fc59e257ec8a62e0e6f
crazydiamond <azhuoyu1@gmail.com>
2024-12-23 16:05:07 +0800

    fix:Create new article

--- article/views.py
+++ article/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import ArticlePost
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
+import markdown
 # Create your views here.
 def article_list(request):
     articles = ArticlePost.objects.all()
@@ -9,6 +12,35 @@ def article_list(request):
 
 def article_detail(request,id):
     article = ArticlePost.objects.get(id=id)
+    article.body = markdown.markdown(article.body,
+                                     extensions=['markdown.extensions.extra',
+                                                 'markdown.extensions.codehilite',])
     context = {'article' : article}
     return render(request,'article/detail.html',context)
+
+def article_create(request):
+    # if User Submit article
+    if request.method == "POST":
+        # store the data into Form Entity
+        article_post_form = ArticlePostForm(data=request.POST)
+        # Check the legality of the data
+        if article_post_form.is_valid():
+            # save the data and don't save it to the database
+            new_article = article_post_form.save(commit=False)
+            # point the id=1 User as the Author
+            new_article.author = User.objects.get(id=1)
+            # save the article to the dataset
+            new_article.save()
+            return redirect("article:article_list")
+        # if the data is illegal, return error information
+        else:
+            return HttpResponse("Wrong content. Please refill the form.")
+    else:
+        article_post_form = ArticlePostForm()
+        context = {'article_post_form': article_post_form}
+        return render(request, 'article/create.html', context)
+
+
+
+
 
